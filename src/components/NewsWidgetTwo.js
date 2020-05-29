ef8c1e736731dc941635349d6155c13d321a0d03
Amish Bharti <47895005+amish1999@users.noreply.github.com>
2020-05-30 04:30:34 +0530

    update widget theme

--- src/components/NewsWidgetTwo.js
+++ src/components/NewsWidgetTwo.js
@@ -4,7 +4,7 @@ export default function NewsWidgetTwo() {
     return (
         <div >
             {/* <h2>News Widget #2</h2> */}
-            <iframe id="rediff_realtime_widget" name="rediff_realtime_widget" src="//realtime.rediff.com/widgets/result?searchbar=false&thumbnail=false&cat=business&numresults=8" scrolling="no" frameborder="0" style={{width: "300px",height:"450px",margin:"auto"}}></iframe>
+            <iframe id="rediff_realtime_widget" name="rediff_realtime_widget" src="//realtime.rediff.com/widgets/result?searchbar=false&thumbnail=false&cat=business&numresults=8" scrolling="no" frameborder="0" style={{width: "300px",height:"420px",margin:"auto", backgroundColor:"#d0d6ab"}}></iframe>
         </div>
     )
 }
