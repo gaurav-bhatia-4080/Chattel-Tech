b5d9730141acb24c2652c7b9903910ed1dcad608
Amish Bharti <47895005+amish1999@users.noreply.github.com>
2020-05-30 04:29:04 +0530

    update theme

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Iframe from 'react-iframe'
 import Widget from './components/Widget'
 import NewsWidgetTwo from './components/NewsWidgetTwo'
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor: "#222831"}}>
       { /* navbar*/ }
       <header className="header">
         <nav className="navbar navbar-expand-lg fixed-top">
           <div className="container">
             { /* Logo*/}
-            <img src={ require("./img/logo.png")} alt="logo" style={{width: 50, height: 50, marginRight: 5, alignSelf: 'flex-start'}} />
-            <a className="navbar-brand" href="#"><h3 style={{color: 'black'}}>Chattel</h3></a>
+            <img src={ require("./img/CHATTEL_cc1.png")} alt="logo" style={{width: 200, height: 80, marginRight: 5, alignSelf: 'flex-start'}} />
+            <a className="navbar-brand" href="#"><h3 style={{color: 'white'}}></h3></a>
             <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><i className="fas fa-bars"></i></button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav ml-auto">
@@ -27,8 +28,8 @@ function App() {
       { /*  Hero Section*/}
       <section className="hero bg-top" id="hero" style={{backgroundImage: 'img/banner-4.png', backgroundSize: '100% 80%'}}>
         <div className="container" style={{textAlign:'center'}}>
-
-              <h2 className="font-weight-bold">BUSINESS INSIGHTS</h2>
+           <br/>
+              <h2 className="font-weight-bold" style={{color: "#fff"}}>BUSINESS INSIGHTS</h2>
               <p className="h4 text-uppercase text-primary">Driven By Artificial Intelligence</p>
               <br />
               <p className="my-4 text-muted">90% of all data in the world was created in last 2 years. It is humanly impossible to collect all relevant data real time without exceptions.
@@ -62,12 +63,12 @@ function App() {
         
       <section className="with-pattern-1" id="services">
         <div className="container" >
-          <p className="h5 text-uppercase text-primary">Our Products</p>
+          <p className="h3 text-uppercase text-primary" style={{color: "#fff"}}>Our Products</p>
           <br />
           
           <div className="row align-items-center mb-5" id = "ai">
             <div className="col-lg-6" id="one"><i className="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
-              <h2 className="font-weight-light">Chattel Intelligent Services</h2>
+              <h2 className="font-weight-light" style={{color: "#fff"}}>Chattel Intelligent Services</h2>
               <p className="font-italic text-muted mb-4">We constantly tries to bring most advanced technologies for our clients and help them trade seamlessly. We provide end to end AI services, developing from scratch to integrating as an product i.e. app or web</p>
               <p id="demo2" className="font-italic text-muted mb-4 collapse">
                 Plug and use API for instant implementation, customized APIs as per requirement. 
@@ -80,10 +81,12 @@ function App() {
           <br /><br /><br />
           <div className="row align-items-center mb-5" id = "news">
             <div className="col-lg-6">
+              
               <NewsWidgetTwo />
+              
             </div>
             <div className="col-lg-6" id="three"><i className="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
-              <h2 className="font-weight-light">Chattel News</h2>
+              <h2 className="font-weight-light" style={{color: "#fff"}}>Chattel News</h2>
               <p className="font-italic text-muted mb-4">We collect information from an ever-expanding sea of sources, de-dupe, curate, and prepare a detailed and succinct executive summary
                 of the news and its subsequent reactions and reactions produced by various digital media tailored to you and your company. </p>
                 <p id="demo1" className="font-italic text-muted mb-4 collapse">
@@ -95,7 +98,7 @@ function App() {
 
           <div className="row align-items-center mb-5" id = "stock">
             <div className="col-lg-6" id="five"><i className="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
-              <h2 className="font-weight-light">Chattel Stock</h2>
+              <h2 className="font-weight-light" style={{color: "#fff"}}>Chattel Stock</h2>
               <p className="font-italic text-muted mb-4">We continuously process the historical performance and the volatility of the stocks and
                 the indices of your interest in order to determine the actions of investors.
                 Our level of data mining and processing enables
@@ -113,7 +116,7 @@ function App() {
           <div className="row align-items-center mb-5" id = "bot">
             <div className="col-lg-6 mb-5 mb-lg-0" id="seven"><img className="img-fluid w-100 px-lg-5 img-responsive" src= {require("./img/img-4.png")} alt="Chattel bot" style={{ width:300 , height:300 }} /></div>
               <div className="col-lg-6" id="five"><i className="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
-                <h2 className="font-weight-light">Chattel Bot</h2>
+                <h2 className="font-weight-light" style={{color: "#fff"}}>Chattel Bot</h2>
                 <p className="font-italic text-muted mb-4">We are continuously trying to overcome inefficiencies of human in rapid fluctuating market. Our bot will ensure 
                 maximum profit with minimum risk using different strategies ranging from moving average, candle pattern, advanced technical indicators like squeezed momentum indicators. 
                 Our technical team can build your customized bot for trading of Equity, Forex, or Cryptocurrency. </p>
@@ -133,12 +136,12 @@ function App() {
       <section className="bg-center py-0" id="about" style={{backgroundImage: "img/service-bg.svg"}}>
         <section className="about py-0">
           <div className="container">
-            <p className="h5 text-uppercase text-primary">About Us</p>
+            <p className="h2 text-uppercase text-primary">About Us</p>
             <br />
             <div className="row pb-5">
               <div className="col-lg-4 col-md-6 mb-4 mb-lg-0">
                 { /*  Services Item*/}
-                <div className="card border-0 shadow rounded-lg py-4 text-left">
+                <div className="card border-0 shadow rounded-lg py-4 text-left" style={{backgroundColor: "#e3ddb8"}}>
                   <div className="card-body p-5">
                       {
                         /* <svg className="svg-icon svg-icon-light" style="width:60px;height:60px;color:#ff904e">
@@ -147,11 +150,11 @@ function App() {
                       }
                   
                   
-                    <h3 className="font-weight-normal h4 my-4">Curation </h3>
-                    <p className="text-small mb-0">We are a "event prone" knowledge gathering and curating approach, discovering
+                    <h3 className="font-weight-normal h4 my-4" style={{color: '#222831'}}>Curation </h3>
+                    <p className="text-small mb-0"><strong>We are a "event prone" knowledge gathering and curating approach, discovering
                       a method of madness and developing perspectives that you can trust.
                       We collect and curate relevant data
-                      from a wide range of sources.</p>
+                      from a wide range of sources.</strong></p>
                   </div>
                 </div>
               </div>
@@ -159,7 +162,7 @@ function App() {
             
               <div className="col-lg-4 col-md-6 mb-4 mb-lg-0">
                 { /*  Services Item*/}
-                <div className="card border-0 shadow rounded-lg py-4 text-left">
+                <div className="card border-0 shadow rounded-lg py-4 text-left" style={{backgroundColor: "#e3ddb8"}}>
                   <div className="card-body p-5">
                     {
                       /*
@@ -168,9 +171,9 @@ function App() {
                         </svg>
                       */
                     }
-                    <h3 className="font-weight-normal h4 my-4">Analytics </h3>
-                    <p className="text-small mb-0">We take complete, and efficient advantage of using Supervised and Unsupervised Machine Learning algorithms
-                      to continually learn from historical data, identify patterns and trends, and apply real-time intelligence.</p>
+                    <h3 className="font-weight-normal h4 my-4" style={{color: '#222831'}}>Analytics </h3>
+                    <p className="text-small mb-0"><strong>We take complete, and efficient advantage of using Supervised and Unsupervised Machine Learning algorithms
+                      to continually learn from historical data, identify patterns and trends, and apply real-time intelligence.</strong></p>
                   </div>
                 </div>
               </div>
@@ -178,7 +181,7 @@ function App() {
 
               <div className="col-lg-4 col-md-6">
                 { /*  Services Item*/}
-                <div className="card border-0 shadow rounded-lg py-4 text-left">
+                <div className="card border-0 shadow rounded-lg py-4 text-left" style={{backgroundColor: "#e3ddb8"}}>
                   <div className="card-body p-5">
                     {
                       /*
@@ -187,9 +190,9 @@ function App() {
                         </svg>
                       */
                     }
-                    <h3 className="font-weight-normal h4 my-4">Insights</h3>
-                    <p className="text-small mb-0">We produce investor perception reports that affect markets predictably; make it worthwhile to
-                      interpret economic and market perceptions to make smarter and more informed investment decisions. </p>
+                    <h3 className="font-weight-normal h4 my-4" style={{color: '#222831'}}>Insights</h3>
+                    <p className="text-small mb-0"><strong>We produce investor perception reports that affect markets predictably; make it worthwhile to
+                      interpret economic and market perceptions to make smarter and more informed investment decisions.<p></p></strong></p>
                   </div>
                 </div>
               </div>
@@ -205,7 +208,7 @@ function App() {
           <div className="colorlib-narrow-content">
             <div className="row">
               <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box">
-                <p className="h5 text-uppercase text-primary">Get in Touch</p>
+                <p className="h3 text-uppercase text-primary">Get in Touch</p>
                 <br />
                 <br />
               </div>
@@ -216,21 +219,21 @@ function App() {
                 <Iframe 
                   url="http://maps.google.com/maps?q=12.9779083,77.6396974&z=15&output=embed"
                   width="400px" 
-                  height="300px"
+                  height="350px"
                   frameBorder="0"
                   allowFullScreen=""
                   tabIndex="0"
-                  style={{border:0,marginRight: 10}}
+                  style={{frameBorder:1,marginRight: 10, paddingTop: '100px'}}
                 
                 />
                 <br />
                 <br />
                 <p className="h5 text-uppercase text-primary" >Chattel Technologies</p>
-                <p className="text-small mb-0">39, NGEF Ln, Binnamangala, 1st Stage, <br />
+                <p className="text-small mb-0" style={{color: '#d0d6ab'}}>39, NGEF Ln, Binnamangala, 1st Stage, <br />
                   Indiranagar, Bengaluru, Karnataka-560038, India</p>
               </div>
-              <div className="col-md-7 col-md-push-1">
-                <div className="row" style={{margin: 10}}>
+              <div className="col-md-7 col-md-push-1 rounded-lg" style={{paddingBottom: '40px'}}>
+                <div className="row rounded-lg" style={{margin: 10, backgroundColor: "#d7e0c1", paddingTop: '20px'}}>
                   <div className="col-md-10 col-md-offset-1 col-md-pull-1 animate-box">
                     <form id="fs-frm" name="simple-contact-form" accept-charset="utf-8" action="https://formspree.io/d3dileep@gmail.com" method="post">
                       <fieldset id="fs-frm-inputs">
@@ -264,13 +267,13 @@ function App() {
 
 
 
-      <footer className="with-pattern-1 position-relative" >
-        <div className="container section-padding-y">
+      <footer className="with-pattern-1 position-relative" style={{backgroundColor: '#1b3152'}}>
+        <div className="container section-padding-y" style={{paddingLeft: '10px'}}>
           <div className="row">
-            <div className="col-lg-3 mb-4 mb-lg-0"> <h4>Chattel</h4> <br />
-              <p className="text-muted">With the passing decade, world market will be virtual and bots will do the business. Are you eqipped with latest technological tool?</p>
+            <div className="col-lg-3 mb-5 mb-lg-0"> <h4>Chattel</h4> <br />
+              <p className="text-muted" style={{fontSize: '1.3em'}}>With the passing decade, world market will be virtual and bots will do the business. Are you eqipped with latest technological tool?</p>
             </div>
-            <div className="col-lg-2 mb-4 mb-lg-0">
+            <div className="col-lg-2 mb-5 mb-lg-0" style={{paddingLeft: '50px'}}>
               <h2 className="h5 mb-4">Quick Links</h2>
               <div className="d-flex">
                 <ul className="list-unstyled d-inline-block mr-4 mb-0">
@@ -281,7 +284,7 @@ function App() {
                 </ul>
               </div>
             </div>
-            <div className="col-lg-2 mb-4 mb-lg-0">
+            <div className="col-lg-2 mb-5 mb-lg-0" style={{paddingLeft: '50px'}}>
               <h2 className="h5 mb-4">Services</h2>
               <div className="d-flex">
                 <ul className="list-unstyled mr-4 mb-0">
@@ -292,7 +295,7 @@ function App() {
                 </ul>
               </div>
             </div>
-            <div className="col-lg-5">
+            <div className="col-lg-5" style={{paddingLeft: '80px'}}>
               <h2 className="h5 mb-4">Contact Info</h2>
               <ul className="list-unstyled mr-4 mb-3">
                 <li className="mb-2 text-muted">Bangalore </li>
@@ -305,10 +308,13 @@ function App() {
                 <li className="list-inline-item"><a className="social-link" href="#"><i className="fab fa-google-plus"></i></a></li>
                 <li className="list-inline-item"><a className="social-link" href="#"><i className="fab fa-instagram"></i></a></li>
               </ul>
+
+                
             </div>
+            
           </div>
         </div>
-
+        
       </footer>
     </div>
   );
