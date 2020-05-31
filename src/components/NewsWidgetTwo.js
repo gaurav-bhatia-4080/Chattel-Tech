4f2b4f88103e9352158ce78f800b11aca61f2ae1
unknown <harsh.gupta1510@gmail.com>
2020-05-31 15:22:58 +0530

    styling for small-screen devices fixed

--- src/components/NewsWidgetTwo.js
+++ src/components/NewsWidgetTwo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function NewsWidgetTwo() {
     return (
-        <div style={{marginTop:'100px'}}>
+        <div>
             {/* <h2>News Widget #2</h2> */}
             <iframe id="rediff_realtime_widget" name="rediff_realtime_widget" src="//realtime.rediff.com/widgets/result?searchbar=false&thumbnail=false&cat=business&numresults=8" scrolling="no" frameborder="0" style={{width: "298px",height:"391px",backgroundColor:"#c8cacf", marginTop:'0px', borderRadius: '10px'}}></iframe>
         </div>
--- src/components/Widget.js
+++ src/components/Widget.js
@@ -18,43 +18,41 @@ function Widget() {
 
 
     return (
-        <div style={{marginLeft:"3em", backgroundColor: '#222831', marginTop: '220px'}}>
+        <div className="stock-widget">
             {/* <h2>Stock Widget #1</h2> */}
-           <div style={{paddingBottom: "5px"}}> 
-            <form onSubmit={handleSubmit}>
-                <input style={{"width":"70%", textAlign: 'center',backgroundColor: '#b0f0f7', border:'0',height:'30px', borderRadius: '8px'}}
-                    type="text"
-                    value={stockSymbol}
-                    list='stocks'
-                    name="stockSymbol"
-                    placeholder="Choose from dropdown or type symbol"
-                    onChange={handleChange}
-                />
-                <button type='submit' style={{border: '0',borderRadius:'8px',marginLeft:'5px', backgroundColor: '#daa850', width: "95px",height:"30px"}}><strong>Check</strong></button>
-                <datalist id="stocks">
-                    <option value="BTCUSD">BTCUSD</option>
-                    <option value="EURUSD">EURUSD</option>
-                    <option value="BTCUSD">BTCUSD</option>
-                    <option value="GBPUSD">GBPUSD</option>
-                    <option value="USDJPY">USDJPY</option>
-                    <option value="XBTUSD">XBTUSD</option>
-                    <option value="AAPL">AAPL</option>
-                    <option value="XAUUSD">XAUUSD</option>
-                    <option value="BTCUSDT">BTCUSDT</option>
-                    <option value="AUDUSD">AUDUSD</option>
-                </datalist>
-            </form>
-            </div>
-            <div >
-                <div >
-                    <TechnicalAnalysis 
-                        key={stockValue ? stockValue : 'NASDAQ:AAPL'}
-                        symbol={stockValue ? stockValue : 'NASDAQ:AAPL'}
-                        interval={"1W"}
-                        colorTheme={"dark"}
-                        isTransparent={"true"}
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input style={{ "width": "70%", textAlign: 'center', backgroundColor: '#b0f0f7', border: '0', height: '30px', borderRadius: '8px' }}
+                        type="text"
+                        value={stockSymbol}
+                        list='stocks'
+                        name="stockSymbol"
+                        placeholder="Choose from dropdown or type symbol"
+                        onChange={handleChange}
                     />
-                </div>
+                    <button type='submit' style={{ border: '0', borderRadius: '8px', marginLeft: '5px', backgroundColor: '#daa850', width: "95px", height: "30px" }}><strong>Check</strong></button>
+                    <datalist id="stocks">
+                        <option value="BTCUSD">BTCUSD</option>
+                        <option value="EURUSD">EURUSD</option>
+                        <option value="BTCUSD">BTCUSD</option>
+                        <option value="GBPUSD">GBPUSD</option>
+                        <option value="USDJPY">USDJPY</option>
+                        <option value="XBTUSD">XBTUSD</option>
+                        <option value="AAPL">AAPL</option>
+                        <option value="XAUUSD">XAUUSD</option>
+                        <option value="BTCUSDT">BTCUSDT</option>
+                        <option value="AUDUSD">AUDUSD</option>
+                    </datalist>
+                </form>
+            </div>
+            <div>
+                <TechnicalAnalysis
+                    key={stockValue ? stockValue : 'NASDAQ:AAPL'}
+                    symbol={stockValue ? stockValue : 'NASDAQ:AAPL'}
+                    interval={"1W"}
+                    colorTheme={"dark"}
+                    isTransparent={"true"}
+                />
             </div>
         </div>
     )
